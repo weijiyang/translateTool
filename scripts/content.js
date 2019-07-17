e12f29aaaa58ccad20f88ffe40e1ae90ed9ecf18
weijiyang <weijiyang@meicai.cn>
2019-07-17 11:12:26 +0800

    iframe不显示 & 显示状态记录 & 悬浮框内容不可选

--- scripts/content.js
+++ scripts/content.js
@@ -10,11 +10,21 @@ function getSelectedText() {
 }
 
 $(function () {
+  if (top.location != location) return
   let createDiv = document.createElement('div')
   createDiv.title = '便捷翻译'
   createDiv.id = 'translateExtension'
   createDiv.innerHTML = '<span>请使用鼠标选中想要翻译字段</span>'
   $('body').append(createDiv)
+  chrome.storage.local.get(['showtranslateExtension'], function(result) {
+    if (result['showtranslateExtension'] == 'false') {
+      console.log('翻译插件保存状态为：隐藏');
+      $('#translateExtension').hide()
+    } else {
+      console.log('翻译插件保存状态为：显示');
+      $('#translateExtension').show()
+    }
+  });
   $('body').mouseup(function () {
     let str = getSelectedText()
     if (str) {
@@ -31,6 +41,15 @@ $(function () {
   $('body').keypress(function(e){
     if (e.altKey && e.which == 339) {
       $('#translateExtension').toggle()
+      if ($('#translateExtension').attr('style') == "display: none;") {
+        chrome.storage.local.set({showtranslateExtension: "false"}, function() {
+            console.log('chrome 本地存储翻译插件隐藏状态');
+        });
+      } else {
+        chrome.storage.local.set({showtranslateExtension: "true"}, function() {
+          console.log('chrome 本地存储翻译插件显示状态');
+        });
+      }
     }
   })
 })
